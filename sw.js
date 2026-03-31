f258763feb764a460b72096c6ea13412e55f0228
lucmillepied98 <lucienmillepied@icloud.com>
2026-03-31 07:05:21 +0200

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -1,20 +1,11 @@
-var CACHE = ‘koinonia-v1’;
-var FILES = [’/’, ‘/index.html’, ‘/manifest.json’];
+var CACHE = ‘koinonia-v3’;
+var FILES = [’/’, ‘/index.html’, ‘/manifest.json’, ‘/icon.png’];
 
 self.addEventListener(‘install’, function(e) {
+self.skipWaiting();
 e.waitUntil(
 caches.open(CACHE).then(function(cache) {
-return cache.addAll(FILES);
-})
-);
-});
-
-self.addEventListener(‘fetch’, function(e) {
-e.respondWith(
-caches.match(e.request).then(function(cached) {
-return cached || fetch(e.request).catch(function() {
-return caches.match(’/index.html’);
-});
+return cache.addAll(FILES).catch(function(){});
 })
 );
 });
@@ -26,6 +17,16 @@ return Promise.all(
 keys.filter(function(k) { return k !== CACHE; })
 .map(function(k) { return caches.delete(k); })
 );
+}).then(function() {
+return self.clients.claim();
+})
+);
+});
+
+self.addEventListener(‘fetch’, function(e) {
+e.respondWith(
+fetch(e.request).catch(function() {
+return caches.match(e.request);
 })
 );
 });
